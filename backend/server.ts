4e72c4998882f53e013c5c98be57c2fb632a1a10
habiberdn <habib.erdian@gmail.com>
2026-07-30 08:26:53 +0700

    first init

---
+++ backend/server.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config({ path: './.env' });
+
+import app from './app';
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
